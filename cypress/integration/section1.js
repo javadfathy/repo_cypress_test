79e1f2c7f4f7044c67f02857f46ea9e5ed3b31df
JAVAD <jwadfthy2@gmail.com>
2022-05-14 00:51:49 +0430

    update 1

---
+++ cypress/integration/section1.js
@@ -0,0 +1,54 @@
+/// <reference type="cypress"/>
+
+
+
+describe("Suite 1", () => {
+    // it ('1- Google search', () => {
+    //     cy.visit("https://google.com")
+    //     cy.get("input[name=q]").type("madareno{enter}")
+    // })
+    
+    
+    // it ('2- test 1', () => {
+    //     cy.visit("https://vuetifyjs.com/")
+    //     cy.title().should('include', "Vuetify")
+    // })
+
+    // it ('3- test', () => {
+    //     cy.visit("http://new.madareno.com")
+    //     cy.get('[style="transition-delay: .9s"] > .SiteHeader-nav-link').type('{enter}') 
+    // })
+
+    // it ('4- test', () => {
+    //     cy.visit("https://vuejs.org/")
+    //     cy.contains("h2","Platinum Sponsors") 
+    //     // cy.get("h2").contains("Platinum Sponsors")
+    // })
+
+
+    // it ('5- test', () => {
+    //     cy.visit("https://digikala.com")
+    //     cy.get(".d-block-md").children("[href='/faq/question/649/']").click()
+    //     cy
+    // })
+
+    // it ('6- test', () => {
+    //     cy.visit("https://google.com")
+    //     cy.get("input[name=q]").type("I'm typing Slow",{delay:0})
+    //     cy.get("input[name=q]").type("{selectAll}{del}")
+    // })
+
+    // it.only ('7- test', () => {
+    //     cy.visit("https://google.com")
+    //     cy.get("input[name=q]").type("Cypress")
+    //         .realPress(["Control","A"])
+    //         .realPress(["Control","X"])
+    //         .realPress(["Control","V"])
+    // })
+
+    
+    it ('8- test', () => {
+        cy.visit("https://google.com")
+        cy.get("input[name=q]").type("Cypress ".repeat(2))
+    })
+})
